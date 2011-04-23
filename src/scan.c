3344077129b1c06585f27e47e457c648e9d5bb9b
Matteo Panella <morpheus@azzurra.org>
2011-04-23 12:14:37 +0200

    Drop support for historical versions of Linux glibc.

--- src/scan.c
+++ src/scan.c
@@ -121,17 +121,6 @@ int kqfd = -1;
 #endif
 
 #ifdef USING_EPOLL
-
-# if !defined(__NR_epoll_create) || !defined(__NR_epoll_ctl) || !defined(__NR_epoll_wait)
-#  include <linux/unistd.h>
-#  define __NR_epoll_create       254
-#  define __NR_epoll_ctl          255
-#  define __NR_epoll_wait         256
-_syscall1(int, epoll_create, int, size)
-_syscall4(int, epoll_ctl, int, epfd, int, op, int, fd, struct epoll_event *, event)
-_syscall4(int, epoll_wait, int, epfd, struct epoll_event *, pevents, int, maxevents, int, timeout)
-# endif
-
 int epfd = -1;
 #endif
 
